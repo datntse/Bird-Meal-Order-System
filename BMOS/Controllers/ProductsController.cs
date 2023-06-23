d39a8d98c1ae1e568f33017a39201b86a9e8031f
CuongPhan <cuongpvpvc@gmail.com>
2023-06-23 10:49:54 +0700

    change controller

--- BMOS/Controllers/ProductsController.cs
+++ BMOS/Controllers/ProductsController.cs
@@ -11,6 +11,8 @@ using Firebase.Auth;
 using Firebase.Storage;
 using BMOS.Models;
 using BMOS.Models.Services;
+using X.PagedList;
+
 namespace BMOS.Controllers
 {
     public class TblProductsController : Controller
@@ -27,125 +29,66 @@ namespace BMOS.Controllers
             _context = context;
         }
 
-        // GET: TblProducts
-        public async Task<IActionResult> Index()
-        {
-            return _context.TblProducts != null ?
-                        View(await _context.TblProducts.ToListAsync()) :
-                        Problem("Entity set 'BmosContext.TblProducts'  is null.");
-        }
-        public async Task<IActionResult> ProductList()
-        {
-            return _context.TblProducts != null ?
-                        View(await _context.TblProducts.ToListAsync()) :
-                        Problem("Entity set 'BmosContext.TblProducts'  is null.");
-        }
+		public async Task<IActionResult> ListProduct(string sortOrder, string currentFilter, string searchString, int? page)
+		{
+			ViewData["SearchParameter"] = searchString;
+			ViewBag.CurrentSort = sortOrder;
+			ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name" : "";
+			ViewData["NameDescSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+			ViewData["PriceSortParm"] = String.IsNullOrEmpty(sortOrder) ? "price" : "";
+			ViewData["PriceDescSortParm"] = String.IsNullOrEmpty(sortOrder) ? "price_desc" : "";
 
-        // GET: TblProducts/Details/5
-        public async Task<IActionResult> Details(string id)
-        {
-            if (id == null || _context.TblProducts == null)
-            {
-                return NotFound();
-            }
+			if (searchString != null)
+			{
+				page = 1;
+			}
+			else
+			{
+				searchString = currentFilter;
+			}
 
-            var tblProduct = await _context.TblProducts
-                .FirstOrDefaultAsync(m => m.ProductId == id);
-            if (tblProduct == null)
-            {
-                return NotFound();
-            }
+			ViewBag.CurrentFilter = searchString;
 
-            return View(tblProduct);
-        }
+			var products = from s in _context.TblProducts
+						   select s;
+			if (!String.IsNullOrEmpty(searchString))
+			{
+				products = products.Where(s => s.Name.Contains(searchString));
+				int count = products.Count();
+				if (count == 0)
+				{
+					ViewBag.Message = "No matches found";
+				}
+				else
+				{
+					ViewBag.Message = "Có " + count + " kết quả tìm kiếm với từ khóa: " + searchString;
+				}
+			}
 
-        // GET: TblProducts/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+			switch (sortOrder)
+			{
+				case "name":
+					products = products.OrderBy(s => s.Name);
+					break;
+				case "name_desc":
+					products = products.OrderByDescending(s => s.Name);
+					break;
+				case "price":
+					products = products.OrderBy(s => s.Price);
+					break;
+				case "price_desc":
+					products = products.OrderByDescending(s => s.Price);
+					break;
+				default:
+					products = products.OrderBy(s => s.ProductId);
+					break;
+			}
+			int pageSize = 8;
+			int pageNumber = (page ?? 1);
+			return View(products.ToPagedList(pageNumber, pageSize));
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProductId,Name, Quantity, Price, Description,Weight,IsAvailable,IsLoved,Status")] TblProduct tblProduct, List<IFormFile> files)
-        {
-
-            string url = "";
-            if (ModelState.IsValid)
-            {
-                url = await FirebaseService.UploadImage(files);
-                _context.Add(tblProduct);
-                TblImage tblImage = new TblImage
-                {
-                    ImageId = Guid.NewGuid().ToString(),
-                    Name = "Product img",
-                    RelationId = tblProduct.ProductId,
-                    Type = "Product",
-                    Url = url
-                };
-                _context.TblImages.Add(tblImage);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-
-            }
-
-            return View(tblProduct);
-        }
-
-
-        // GET: TblProducts/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null || _context.TblProducts == null)
-            {
-                return NotFound();
-            }
-
-            var tblProduct = await _context.TblProducts.FindAsync(id);
-            if (tblProduct == null)
-            {
-                return NotFound();
-            }
-            return View(tblProduct);
-        }
-
-        // POST: TblProducts/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("ProductId,Name,Quantity,Description,Weight,IsAvailable,IsLoved,Status")] TblProduct tblProduct)
-        {
-            if (id != tblProduct.ProductId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(tblProduct);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TblProductExists(tblProduct.ProductId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(tblProduct);
-        }
-
-        // GET: TblProducts/Delete/5
-        public async Task<IActionResult> Delete(string id)
+		public async Task<IActionResult> Details(string id)
         {
             if (id == null || _context.TblProducts == null)
             {
@@ -162,24 +105,7 @@ namespace BMOS.Controllers
             return View(tblProduct);
         }
 
-        // POST: TblProducts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            if (_context.TblProducts == null)
-            {
-                return Problem("Entity set 'BmosContext.TblProducts'  is null.");
-            }
-            var tblProduct = await _context.TblProducts.FindAsync(id);
-            if (tblProduct != null)
-            {
-                _context.TblProducts.Remove(tblProduct);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+       
 
         private bool TblProductExists(string id)
         {
