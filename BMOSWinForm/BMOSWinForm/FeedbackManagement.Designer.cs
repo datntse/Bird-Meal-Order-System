0bcff1fbb248a8ec5113135cd597ba8d48f99e2e
CuongPhan <cuongpvpvc@gmail.com>
2023-06-26 22:09:27 +0700

    upload feedback 95%

--- BMOSWinForm/BMOSWinForm/FeedbackManagement.Designer.cs
+++ BMOSWinForm/BMOSWinForm/FeedbackManagement.Designer.cs
@@ -34,24 +34,25 @@
             button3 = new System.Windows.Forms.Button();
             btndelete = new System.Windows.Forms.Button();
             dgvFeedbackList = new System.Windows.Forms.DataGridView();
+            idfb = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            idpro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            content = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            star = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            tblFeedbackBindingSource1 = new System.Windows.Forms.BindingSource(components);
             tblFeedbackBindingSource = new System.Windows.Forms.BindingSource(components);
             groupBox1 = new System.Windows.Forms.GroupBox();
+            comboBox1 = new System.Windows.Forms.ComboBox();
             btrs = new System.Windows.Forms.Button();
             label1 = new System.Windows.Forms.Label();
             searchBtn = new System.Windows.Forms.Button();
             txtSearch = new System.Windows.Forms.TextBox();
             txtdetails = new System.Windows.Forms.Button();
-            tblFeedbackBindingSource1 = new System.Windows.Forms.BindingSource(components);
-            feedbackIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            productIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            productDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            contentDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            starDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            dateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dgvFeedbackList).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)tblFeedbackBindingSource1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)tblFeedbackBindingSource).BeginInit();
             groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)tblFeedbackBindingSource1).BeginInit();
             SuspendLayout();
             // 
             // label2
@@ -69,15 +70,15 @@
             txt_id.Location = new System.Drawing.Point(11, 520);
             txt_id.Margin = new System.Windows.Forms.Padding(2);
             txt_id.Name = "txt_id";
-            txt_id.Size = new System.Drawing.Size(97, 27);
+            txt_id.Size = new System.Drawing.Size(132, 27);
             txt_id.TabIndex = 9;
             // 
             // button3
             // 
-            button3.Location = new System.Drawing.Point(816, 508);
+            button3.Location = new System.Drawing.Point(941, 508);
             button3.Margin = new System.Windows.Forms.Padding(2);
             button3.Name = "button3";
-            button3.Size = new System.Drawing.Size(90, 49);
+            button3.Size = new System.Drawing.Size(124, 49);
             button3.TabIndex = 10;
             button3.Text = "Thoát";
             button3.UseVisualStyleBackColor = true;
@@ -85,12 +86,12 @@
             // 
             // btndelete
             // 
-            btndelete.Location = new System.Drawing.Point(229, 514);
+            btndelete.Location = new System.Drawing.Point(303, 515);
             btndelete.Margin = new System.Windows.Forms.Padding(2);
             btndelete.Name = "btndelete";
-            btndelete.Size = new System.Drawing.Size(94, 37);
+            btndelete.Size = new System.Drawing.Size(118, 37);
             btndelete.TabIndex = 7;
-            btndelete.Text = "del";
+            btndelete.Text = "Xóa";
             btndelete.UseVisualStyleBackColor = true;
             btndelete.Click += btndelete_Click;
             // 
@@ -98,12 +99,10 @@
             // 
             dgvFeedbackList.AllowUserToAddRows = false;
             dgvFeedbackList.AllowUserToDeleteRows = false;
-            dgvFeedbackList.AutoGenerateColumns = false;
             dgvFeedbackList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             dgvFeedbackList.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
             dgvFeedbackList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvFeedbackList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { feedbackIdDataGridViewTextBoxColumn, productIdDataGridViewTextBoxColumn, productDataGridViewTextBoxColumn, contentDataGridViewTextBoxColumn, starDataGridViewTextBoxColumn, dateDataGridViewTextBoxColumn });
-            dgvFeedbackList.DataSource = tblFeedbackBindingSource1;
+            dgvFeedbackList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { idfb, idpro, name, content, star, date });
             dgvFeedbackList.Location = new System.Drawing.Point(11, 99);
             dgvFeedbackList.Margin = new System.Windows.Forms.Padding(2);
             dgvFeedbackList.Name = "dgvFeedbackList";
@@ -111,9 +110,66 @@
             dgvFeedbackList.RowHeadersWidth = 62;
             dgvFeedbackList.RowTemplate.Height = 33;
             dgvFeedbackList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            dgvFeedbackList.Size = new System.Drawing.Size(895, 383);
+            dgvFeedbackList.Size = new System.Drawing.Size(1054, 383);
             dgvFeedbackList.TabIndex = 6;
             dgvFeedbackList.CellContentClick += dgvFeedbackList_CellContentClick;
+            // 
+            // idfb
+            // 
+            idfb.DataPropertyName = "idfb";
+            idfb.FillWeight = 40F;
+            idfb.HeaderText = "ID Feedback";
+            idfb.MinimumWidth = 6;
+            idfb.Name = "idfb";
+            idfb.ReadOnly = true;
+            // 
+            // idpro
+            // 
+            idpro.DataPropertyName = "idpro";
+            idpro.FillWeight = 40F;
+            idpro.HeaderText = "ID sản phẩm";
+            idpro.MinimumWidth = 6;
+            idpro.Name = "idpro";
+            idpro.ReadOnly = true;
+            // 
+            // name
+            // 
+            name.DataPropertyName = "name";
+            name.HeaderText = "Tên sản phẩm";
+            name.MinimumWidth = 6;
+            name.Name = "name";
+            name.ReadOnly = true;
+            // 
+            // content
+            // 
+            content.DataPropertyName = "content";
+            content.FillWeight = 150F;
+            content.HeaderText = "Nội dung đánh giá";
+            content.MinimumWidth = 6;
+            content.Name = "content";
+            content.ReadOnly = true;
+            // 
+            // star
+            // 
+            star.DataPropertyName = "star";
+            star.FillWeight = 30F;
+            star.HeaderText = "Sao";
+            star.MinimumWidth = 6;
+            star.Name = "star";
+            star.ReadOnly = true;
+            // 
+            // date
+            // 
+            date.DataPropertyName = "date";
+            date.FillWeight = 80F;
+            date.HeaderText = "Thời gian";
+            date.MinimumWidth = 6;
+            date.Name = "date";
+            date.ReadOnly = true;
+            // 
+            // tblFeedbackBindingSource1
+            // 
+            tblFeedbackBindingSource1.DataSource = typeof(Repository.Models.Entities.TblFeedback);
             // 
             // tblFeedbackBindingSource
             // 
@@ -121,6 +177,7 @@
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(comboBox1);
             groupBox1.Controls.Add(btrs);
             groupBox1.Controls.Add(label1);
             groupBox1.Controls.Add(searchBtn);
@@ -129,14 +186,25 @@
             groupBox1.Margin = new System.Windows.Forms.Padding(2);
             groupBox1.Name = "groupBox1";
             groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            groupBox1.Size = new System.Drawing.Size(895, 70);
+            groupBox1.Size = new System.Drawing.Size(1054, 70);
             groupBox1.TabIndex = 5;
             groupBox1.TabStop = false;
             groupBox1.Text = "Manage Feedback";
             // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Tất cả sản phẩm", "A-Z", "Z-A", "Số sao tăng dần", "Số sao giảm dần" });
+            comboBox1.Location = new System.Drawing.Point(654, 26);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new System.Drawing.Size(151, 28);
+            comboBox1.TabIndex = 4;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
             // btrs
             // 
-            btrs.Location = new System.Drawing.Point(769, 16);
+            btrs.Location = new System.Drawing.Point(941, 14);
             btrs.Name = "btrs";
             btrs.Size = new System.Drawing.Size(96, 50);
             btrs.TabIndex = 3;
@@ -156,7 +224,7 @@
             // 
             // searchBtn
             // 
-            searchBtn.Location = new System.Drawing.Point(652, 16);
+            searchBtn.Location = new System.Drawing.Point(825, 15);
             searchBtn.Margin = new System.Windows.Forms.Padding(2);
             searchBtn.Name = "searchBtn";
             searchBtn.Size = new System.Drawing.Size(99, 50);
@@ -175,71 +243,19 @@
             // 
             // txtdetails
             // 
-            txtdetails.Location = new System.Drawing.Point(116, 514);
+            txtdetails.Location = new System.Drawing.Point(164, 514);
             txtdetails.Name = "txtdetails";
-            txtdetails.Size = new System.Drawing.Size(94, 38);
+            txtdetails.Size = new System.Drawing.Size(122, 38);
             txtdetails.TabIndex = 11;
             txtdetails.Text = "Chi Tiết";
             txtdetails.UseVisualStyleBackColor = true;
             txtdetails.Click += txtdetails_Click;
             // 
-            // tblFeedbackBindingSource1
-            // 
-            tblFeedbackBindingSource1.DataSource = typeof(Repository.Models.Entities.TblFeedback);
-            // 
-            // feedbackIdDataGridViewTextBoxColumn
-            // 
-            feedbackIdDataGridViewTextBoxColumn.DataPropertyName = "FeedbackId";
-            feedbackIdDataGridViewTextBoxColumn.HeaderText = "FeedbackId";
-            feedbackIdDataGridViewTextBoxColumn.MinimumWidth = 6;
-            feedbackIdDataGridViewTextBoxColumn.Name = "feedbackIdDataGridViewTextBoxColumn";
-            feedbackIdDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // productIdDataGridViewTextBoxColumn
-            // 
-            productIdDataGridViewTextBoxColumn.DataPropertyName = "ProductId";
-            productIdDataGridViewTextBoxColumn.HeaderText = "ProductId";
-            productIdDataGridViewTextBoxColumn.MinimumWidth = 6;
-            productIdDataGridViewTextBoxColumn.Name = "productIdDataGridViewTextBoxColumn";
-            productIdDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // productDataGridViewTextBoxColumn
-            // 
-            productDataGridViewTextBoxColumn.DataPropertyName = "Product.Name";
-            productDataGridViewTextBoxColumn.HeaderText = "Product";
-            productDataGridViewTextBoxColumn.MinimumWidth = 6;
-            productDataGridViewTextBoxColumn.Name = "productDataGridViewTextBoxColumn";
-            productDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // contentDataGridViewTextBoxColumn
-            // 
-            contentDataGridViewTextBoxColumn.DataPropertyName = "Content";
-            contentDataGridViewTextBoxColumn.HeaderText = "Content";
-            contentDataGridViewTextBoxColumn.MinimumWidth = 6;
-            contentDataGridViewTextBoxColumn.Name = "contentDataGridViewTextBoxColumn";
-            contentDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // starDataGridViewTextBoxColumn
-            // 
-            starDataGridViewTextBoxColumn.DataPropertyName = "Star";
-            starDataGridViewTextBoxColumn.HeaderText = "Star";
-            starDataGridViewTextBoxColumn.MinimumWidth = 6;
-            starDataGridViewTextBoxColumn.Name = "starDataGridViewTextBoxColumn";
-            starDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // dateDataGridViewTextBoxColumn
-            // 
-            dateDataGridViewTextBoxColumn.DataPropertyName = "Date";
-            dateDataGridViewTextBoxColumn.HeaderText = "Date";
-            dateDataGridViewTextBoxColumn.MinimumWidth = 6;
-            dateDataGridViewTextBoxColumn.Name = "dateDataGridViewTextBoxColumn";
-            dateDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
             // FeedbackManagement
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(910, 595);
+            ClientSize = new System.Drawing.Size(1178, 592);
             Controls.Add(txtdetails);
             Controls.Add(label2);
             Controls.Add(txt_id);
@@ -252,10 +268,10 @@
             Text = "FeedbackManagement";
             Load += FeedbackManagement_Load;
             ((System.ComponentModel.ISupportInitialize)dgvFeedbackList).EndInit();
+            ((System.ComponentModel.ISupportInitialize)tblFeedbackBindingSource1).EndInit();
             ((System.ComponentModel.ISupportInitialize)tblFeedbackBindingSource).EndInit();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)tblFeedbackBindingSource1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -278,9 +294,17 @@
         private System.Windows.Forms.BindingSource tblFeedbackBindingSource1;
         private System.Windows.Forms.DataGridViewTextBoxColumn feedbackIdDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn productIdDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn productDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn contentDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn starDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn dateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn userIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productDataGridViewTextBoxColumn;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idfb;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idpro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn content;
+        private System.Windows.Forms.DataGridViewTextBoxColumn star;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date;
     }
 }
