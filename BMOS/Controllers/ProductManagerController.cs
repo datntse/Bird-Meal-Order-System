23c998808c528714b0882c8275b45fb867b557c5
datntse <dat.nt271102@gmail.com>
2023-07-02 11:23:11 +0700

    upload project

--- BMOS/Controllers/ProductManagerController.cs
+++ BMOS/Controllers/ProductManagerController.cs
@@ -102,7 +102,7 @@ namespace BMOS.Controllers
         }
 
         // POST: ProductManager/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // To protect from overposting attacks,  enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
