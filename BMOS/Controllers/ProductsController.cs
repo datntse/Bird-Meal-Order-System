70c1f24e5aa4e4ff3f044ce4e99cefe79a4c2c51
datntse <dat.nt271102@gmail.com>
2023-07-19 00:02:22 +0700

    update voucher management

--- BMOS/Controllers/ProductsController.cs
+++ BMOS/Controllers/ProductsController.cs
@@ -66,7 +66,6 @@ namespace BMOS.Controllers
 			ViewData["Feedback"] = feedbackList.ToList();
 
 
-
 			var productItem = from product in _context.TblProducts
 							  from image in _context.TblImages
 							  where product.ProductId == image.RelationId
@@ -74,10 +73,13 @@ namespace BMOS.Controllers
 							  {
 								  ProductId = product.ProductId,
 								  Name = product.Name,
+                                  Quantity = product.Quantity,
 								  Price = product.Price,
 								  Description = product.Description,
 								  Weight = product.Weight,
 								  IsLoved = product.IsLoved,
+								  SoldQuantity = product.SoldQuantity,
+								  Status = product.Status,
 								  UrlImage = image.Url,
 								  relatedProductModels = _listProductRelated
 							  };
@@ -188,7 +190,7 @@ namespace BMOS.Controllers
                                ProductId = product.ProductId,
                                Name = product.Name,
                                Price = product.Price,
-							   type = product.Type,
+							   Type = product.Type,
                                Description = product.Description,
                                Weight = product.Weight,
                                IsLoved = product.IsLoved,
@@ -196,7 +198,7 @@ namespace BMOS.Controllers
                                relatedProductModels = null
                            };
 
-			var result = products.Where(x => x.type == "1").ToList();
+			var result = products.Where(x => x.Type == "1").ToList();
             if (!String.IsNullOrEmpty(searchString))
             {
                 result = result.Where(s => s.Name.Contains(searchString)).ToList();
@@ -262,7 +264,7 @@ namespace BMOS.Controllers
                                ProductId = product.ProductId,
                                Name = product.Name,
                                Price = product.Price,
-                               type = product.Type,
+                               Type = product.Type,
                                Description = product.Description,
                                Weight = product.Weight,
                                IsLoved = product.IsLoved,
@@ -270,7 +272,7 @@ namespace BMOS.Controllers
                                relatedProductModels = null
                            };
 
-            var result = products.Where(x => x.type == "2").ToList();
+            var result = products.Where(x => x.Type == "2").ToList();
             if (!String.IsNullOrEmpty(searchString))
             {
                 result = result.Where(s => s.Name.Contains(searchString)).ToList();
@@ -327,9 +329,9 @@ namespace BMOS.Controllers
 
             ViewBag.CurrentFilter = searchString;
 
-			
 
-			var products = from product in _context.TblProducts
+
+            var products = from product in _context.TblProducts
                            from image in _context.TblImages
                            where product.ProductId.Equals(image.RelationId)
                            select new ProductInfoModel()
@@ -337,15 +339,16 @@ namespace BMOS.Controllers
                                ProductId = product.ProductId,
                                Name = product.Name,
                                Price = product.Price,
-                               type = product.Type,
+                               Type = product.Type,
                                Description = product.Description,
                                Weight = product.Weight,
                                IsLoved = product.IsLoved,
+                       
                                UrlImage = image.Url,
                                relatedProductModels = null
                            };
 
-            var result = products.Where(x => x.type == "3").ToList();
+            var result = products.Where(x => x.Type == "3").ToList();
             if (!String.IsNullOrEmpty(searchString))
             {
                 result = result.Where(s => s.Name.Contains(searchString)).ToList();
--- BMOS/Controllers/VoucherCodesController.cs
+++ BMOS/Controllers/VoucherCodesController.cs
@@ -1,11 +1,12 @@
 ﻿using BMOS.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BMOS.Controllers
 {
-	public class VoucherCodesController : Controller
-	{
+    public class VoucherCodesController : Controller
+    {
         // GET: VoucherCodesController
         private readonly BmosContext _context;
 
@@ -14,89 +15,117 @@ namespace BMOS.Controllers
             _context = context;
         }
         public ActionResult VoucherManager()
-		{
-			return View();
-		}
+        {
+            return View(_context.TblVoucherCodes.ToList());
+        }
 
-		// GET: VoucherCodesController/Details/5
-		public ActionResult Details(int id)
-		{
-			return View();
-		}
 
-		// GET: VoucherCodesController/Create
-		public ActionResult Create()
-		{
-			return View();
-		}
 
-		// POST: VoucherCodesController/Create
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Create(TblVoucherCode model)
-		{
-			try
-			{
-				var voucher = new TblVoucherCode
-				{
-					VoucherId = model.VoucherId,
-					VoucherCode = model.VoucherCode,
-					Value = model.Value,
-					Quantity = model.Quantity,
-					Used = model.Used,
-					Status = model.Status,
-				};
+        // GET: VoucherCodesController/Create
+        public ActionResult Create()
+        {
 
-				_context.TblVoucherCodes.Add(voucher);	
-				_context.SaveChanges();
-				return RedirectToAction(nameof(Index));
-			}
-			catch
-			{
-				return View();
-			}
-		}
+            return View();
+        }
 
-		// GET: VoucherCodesController/Edit/5
-		public ActionResult Edit(int id)
-		{
-			return View();
-		}
+        // POST: VoucherCodesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("VoucherCode, Value, Quantity")] TblVoucherCode model)
+        {
+            ModelState.Remove("VoucherId");
+            ModelState.Remove("Used");
+            ModelState.Remove("Status");
+            if (ModelState.IsValid) { 
+                var voucherNum = _context.TblVoucherCodes.Count(x => x.VoucherCode != null);
+                voucherNum++;
+                var voucher = new TblVoucherCode
+                {
+                    VoucherId = "voucher" + voucherNum,
+                    VoucherCode = model.VoucherCode,
+                    Value = model.Value,
+                    Quantity = model.Quantity,
+                    Used = 0,
+                    Status = true,
+                };
+                _context.TblVoucherCodes.Add(voucher);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(VoucherManager));
+            }
+            return View();
+          
+        }
 
-		// POST: VoucherCodesController/Edit/5
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Edit(int id, IFormCollection collection)
-		{
-			try
-			{
-				return RedirectToAction(nameof(Index));
-			}
-			catch
-			{
-				return View();
-			}
-		}
+        // GET: VoucherCodesController/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.TblVoucherCodes == null)
+            {
+                return NotFound();
+            }
 
-		// GET: VoucherCodesController/Delete/5
-		public ActionResult Delete(int id)
-		{
-			return View();
-		}
+            var TblVoucherCodes = await _context.TblVoucherCodes.FindAsync(id);
+            if (TblVoucherCodes == null)
+            {
+                return NotFound();
+            }
+            return View(TblVoucherCodes);
+        }
+        // POST: VoucherCodesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("VoucherId, VoucherCode, Value, Quantity, Used, Status")] TblVoucherCode tblVoucherCode)
+        {
+            if (id != tblVoucherCode.VoucherId)
+            {
+                return NotFound();
+            }
 
-		// POST: VoucherCodesController/Delete/5
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Delete(int id, IFormCollection collection)
-		{
-			try
-			{
-				return RedirectToAction(nameof(Index));
-			}
-			catch
-			{
-				return View();
-			}
-		}
-	}
+            if (ModelState.IsValid)
+            {
+
+                _context.Update(tblVoucherCode);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(VoucherManager));
+            }
+            return View(tblVoucherCode);
+        }
+
+        // GET: VoucherCodesController/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null || _context.TblVoucherCodes == null)
+            {
+                return NotFound();
+            }
+
+            var tblVoucherCode = await _context.TblVoucherCodes
+                .FirstOrDefaultAsync(m => m.VoucherId.Equals(id));
+            if (tblVoucherCode == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblVoucherCode);
+        }
+
+        // POST: VoucherCodesController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.TblVoucherCodes == null)
+            {
+                return Problem("Entity set 'BmosContext.TblBlogs'  is null.");
+            }
+            var tblVoucher = await _context.TblVoucherCodes.FindAsync(id);
+            if (tblVoucher != null)
+            {
+                _context.TblVoucherCodes.Remove(tblVoucher);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("VoucherManager");
+        }
+    }
 }
